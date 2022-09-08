a1ff81b09af474fd13c942b4ef835e5891bbfa50
AleksanderRadziszewski <radziszewski.aleksander@gmail.com>
2022-09-08 21:25:53 +0200

    database setup

---
+++ src/entity/contact-info.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Employee } from './employee.entity';
+import { Task } from './task.entity';
+
+@Entity()
+export class ContactInfo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  employeeId: number;
+
+  @OneToOne(() => Employee, (employee) => employee.contactInfo)
+  @JoinColumn()
+  employee: Employee;
+
+  @OneToMany(() => Task, (task) => task.employee)
+  tasks: Task[];
+}
